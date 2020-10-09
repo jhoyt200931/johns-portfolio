0fbd93cd9ccc6044a15a1843caf765576d74abd2
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-10-08 23:15:19 -0400

    removed preview photo

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
+
 const Body = () => {
-
-
-
     return (
         <div>
             <h2>Projects</h2>
             <div>
             <a href='https://rick-and-morty-characters.vercel.app/'>Rick and Morty Character Page</a>
-            <img src='./Photos/RandMpreview.png' alt='Rick and Morty page preview' />
             </div>
         </div>
     )
