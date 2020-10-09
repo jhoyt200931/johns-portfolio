88ccefcf0746fedae0ada5efcf0a5771488e1ead
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-10-08 23:01:05 -0400

    building details of portfolio page, body and header

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-
 const Body = () => {
+
+
+
     return (
         <div>
-
+            <h2>Projects</h2>
+            <div>
+            <a href='https://rick-and-morty-characters.vercel.app/'>Rick and Morty Character Page</a>
+            <img src='./R&Mpreview.png' alt='Rick and Morty page preview' />
+            </div>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-
+            <h1>Johnathan Hoyt's Portfolio</h1>
+            <p>Contact Me:</p>
+            <p>Email: jhoyt200931@yahoo.com</p>
         </div>
     )
 }
